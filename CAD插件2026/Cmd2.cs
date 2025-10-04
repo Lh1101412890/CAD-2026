209c9f5648f48c6c0e979290d3c60a27ed6b2b11
Lh1101412890 <75879631+Lh1101412890@users.noreply.github.com>
2025-10-04 14:57:11 +0800

    Update Cmd2.cs

--- CAD插件2026/Cmd2.cs
+++ CAD插件2026/Cmd2.cs
@@ -10,6 +10,7 @@ using Autodesk.AutoCAD.Runtime;
 
 namespace CAD插件2026
 {
+    // 读取“模型空间”块表记录
     public class Cmd2
     {
         [CommandMethod("Cmd2")]
@@ -18,10 +19,13 @@ namespace CAD插件2026
             Document document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
             Database database = document.Database;
 
+            // 读取和写入数据都需要开启事务
             using Transaction transaction = database.TransactionManager.StartTransaction();
             BlockTable blockTable = transaction.GetObject(database.BlockTableId, OpenMode.ForRead) as BlockTable;
-            BlockTableRecord record = transaction.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead) as BlockTableRecord;
+            BlockTableRecord modelSpace = transaction.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead) as BlockTableRecord;
 
+            // 开启事务后立即加一句提交，避免bug
+            // 只读取数据可以不提交
             transaction.Commit();
         }
     }
