13c026ee1c4c9de4c6183cfb5648985bb39184ee
鸿 李 <1101412890@qq.com>
2025-10-08 20:57:30 +0800

    CAD应用程序、文档窗口、视图

---
+++ CAD插件2026/Commands/Cmd4.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+
+using CADApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace CAD插件2026.Commands
+{
+    public class Cmd4
+    {
+        [CommandMethod(nameof(Cmd4))]
+        public static void Command()
+        {
+            CADApp.MainWindow.WindowState = Window.State.Normal;
+            //CADApp.MainWindow.WindowState = Window.State.Maximized;
+            //CADApp.MainWindow.WindowState = Window.State.Minimized;
+
+            // 设置应用程序窗口的位置
+            CADApp.MainWindow.DeviceIndependentLocation = new System.Windows.Point(100, 100);
+            // 设置应用程序窗口的大小
+            CADApp.MainWindow.DeviceIndependentSize = new System.Windows.Size(1400, 800);
+
+            MessageBox.Show("改变尺寸");
+
+            CADApp.MainWindow.Visible = false;
+            MessageBox.Show("CAD隐藏了");
+            CADApp.MainWindow.Visible = true;
+
+            Size size = CADApp.MainWindow.GetSize();
+            CADApp.DocumentManager.MdiActiveDocument.Editor.WriteMessage(size.ToString());
+        }
+    }
+}
---
+++ CAD插件2026/Commands/DocWindow.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using CADApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace CAD插件2026.Commands
+{
+    public class DocWindow
+    {
+        [CommandMethod(nameof(DocWindow))]
+        public static void Command()
+        {
+            Document document = CADApp.DocumentManager.MdiActiveDocument;
+            Editor editor = document.Editor;
+            //document.Window.WindowState = Window.State.Normal;
+            //document.Window.DeviceIndependentSize = new System.Windows.Size(800, 600);
+            //document.Window.DeviceIndependentLocation = new System.Windows.Point(200, 200);
+
+            using (ViewTableRecord view = editor.GetCurrentView())
+            {
+                view.CenterPoint += new Vector2d(100, 0);
+                view.Height = 400;
+                view.Width = 400;
+                editor.SetCurrentView(view);
+            }
+
+            CADApp.UpdateScreen();
+            editor.UpdateScreen();
+
+            editor.Regen();
+        }
+    }
+}
