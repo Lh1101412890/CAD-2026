fe34e4160a238a2df058f12be7f904ea3450db40
鸿 李 <1101412890@qq.com>
2025-10-08 22:24:46 +0800

    文档操作

---
+++ CAD插件2026/Commands/CloseDoc.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+using CADApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace CAD插件2026.Commands
+{
+    public class CloseDoc
+    {
+        [CommandMethod(nameof(CloseDoc), CommandFlags.Session)]
+        public static void Command()
+        {
+            //foreach (Document item in CADApp.DocumentManager)
+            //{
+            //    item.CloseAndSave(item.Name);
+            //    item.CloseAndDiscard();
+            //}
+
+            // 2004: DwgVersion.AC1800
+            // 2007: DwgVersion.AC1021
+            // 2010: DwgVersion.AC1024
+            // 2013: DwgVersion.AC1027
+            // 2018: DwgVersion.AC1032
+            Document document = CADApp.DocumentManager.MdiActiveDocument;
+            Database database = document.Database;
+            database.SaveAs("newName", DwgVersion.Current);
+        }
+    }
+}
---
+++ CAD插件2026/Commands/NewDoc.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+
+using CADApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace CAD插件2026.Commands
+{
+    public class NewDoc
+    {
+        [CommandMethod(nameof(NewDoc))]
+        public static void Command()
+        {
+            //CADApp.DocumentManager.MdiActiveDocument = CADApp.DocumentManager.Add("acad.dwt");
+
+            CADApp.DocumentManager.MdiActiveDocument = CADApp.DocumentManager.Open("C:\\Users\\11014\\Desktop\\Drawing4.dwg", false);
+        }
+    }
+}
