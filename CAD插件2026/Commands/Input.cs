b3dcc7ad4e4a6417ebee4a90a7c6bddeefc4bf33
鸿 李 <1101412890@qq.com>
2025-10-13 22:24:21 +0800

    命令行、提示用户输入

---
+++ CAD插件2026/Commands/Input.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+using CADApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace CAD插件2026.Commands
+{
+    public class Input
+    {
+        [CommandMethod(nameof(Input))]
+        public static void Command()
+        {
+            Document document = CADApp.DocumentManager.MdiActiveDocument;
+            Editor editor = document.Editor;
+            //PromptPointResult promptPointResult = editor.GetPoint("\n请输入点");
+            //if (promptPointResult.Status != PromptStatus.OK)
+            //{
+            //    return;
+            //}
+            //Autodesk.AutoCAD.Geometry.Point3d value = promptPointResult.Value;
+            //editor.WriteMessage("\n" + value.ToString());
+
+            //PromptPointOptions options = new PromptPointOptions("\n请输入点")
+            //{
+            //    LimitsChecked = true
+            //};
+            //PromptPointResult promptPointResult = editor.GetPoint(options);
+            //if (promptPointResult.Status != PromptStatus.OK)
+            //{
+            //    return;
+            //}
+            //editor.WriteMessage(promptPointResult.Value.ToString());
+
+            PromptKeywordOptions options = new PromptKeywordOptions("请选择模式[模式1(1)/模式2(2)/取消(U)]")
+            {
+                AppendKeywordsToMessage = true
+            };
+            options.Keywords.Add("1");
+            options.Keywords.Add("2");
+            options.Keywords.Add("U");
+            PromptResult promptResult = editor.GetKeywords(options);
+            if (promptResult.Status != PromptStatus.OK)
+            {
+                return;
+            }
+            editor.WriteMessage(promptResult.StringResult);
+        }
+    }
+}
---
+++ CAD插件2026/Commands/SendCommand.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+
+using CADApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace CAD插件2026.Commands
+{
+    public class SendCommand
+    {
+        [CommandMethod(nameof(SendCommand))]
+        public static void Command()
+        {
+            Document document = CADApp.DocumentManager.MdiActiveDocument;
+            document.SendStringToExecute("\x03", true, false, true);
+        }
+    }
+}
