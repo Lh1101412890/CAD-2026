afc6154ca2df10999402b4cf9ac38f987e007810
鸿 李 <1101412890@qq.com>
2025-11-03 21:14:46 +0800

    Ribbon功能区-深度剖析

--- CAD插件2026/Commands/AddRibbon.cs
+++ CAD插件2026/Commands/AddRibbon.cs
@@ -18,16 +18,27 @@ namespace CAD插件2026.Commands
             RibbonTab ribbonTab = ribbon.FindTab("MyPluginTab");
             if (ribbonTab is not null)
             {
+                ribbon.ActiveTab = ribbonTab;
                 return;
             }
             RibbonTab tab = new()
             {
                 Title = "我的插件",
-                Id = "MyPluginTab"
+                Id = "MyPluginTab",
+            };
+            RibbonTab tab2 = new()
+            {
+                Title = "我的插件2",
+                Id = "MyPluginTab2",
+                IsContextualTab = true,
+                IsVisible = false
             };
             ribbon.Tabs.Add(tab);
+            ribbon.Tabs.Add(tab2);
             RibbonPanel panel = new();
             tab.Panels.Add(panel);
+            tab2.Panels.Add(panel);
+            ribbon.Tabs[0].Panels.Add(panel);
 
             RibbonPanelSource panelSource = new()
             {
@@ -52,7 +63,7 @@ namespace CAD插件2026.Commands
                 Id = "MyPluginButton",
                 ShowText = true,
                 Size = RibbonItemSize.Standard,
-                Orientation = System.Windows.Controls.Orientation.Vertical,
+                Orientation = System.Windows.Controls.Orientation.Horizontal,
                 Image = new BitmapImage(uri1),
                 LargeImage = new BitmapImage(uri2),
                 ShowImage = true,
@@ -66,10 +77,19 @@ namespace CAD插件2026.Commands
                 Id = "MyPluginButton",
                 ShowText = true,
                 Size = RibbonItemSize.Large,
-                Orientation = System.Windows.Controls.Orientation.Vertical,
+                Orientation = System.Windows.Controls.Orientation.Horizontal,
                 Image = new BitmapImage(uri1),
                 LargeImage = new BitmapImage(uri2),
             };
+
+            RibbonRowPanel rowPanel = new RibbonRowPanel();
+            rowPanel.Items.Add(button1);
+            rowPanel.Items.Add(new RibbonRowBreak());
+            rowPanel.Items.Add(button1);
+            rowPanel.Items.Add(new RibbonRowBreak());
+            rowPanel.Items.Add(button1);
+            panelSource.Items.Add(rowPanel);
+            panelSource.Items.Add(new RibbonPanelBreak());
             panelSource.Items.Add(button1);
             panelSource.Items.Add(button2);
         }
